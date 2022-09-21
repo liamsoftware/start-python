53a0510e0c220b9c39cc579c1fea736154605f4f
LiamHayes1 <liamhayes@live.ie>
2022-09-21 15:15:30 +0100

    update add pass by ref and pass by value file

--- PassBy.py
+++ PassBy.py
@@ -4,12 +4,29 @@
 # Python is pass by reference.
 # The below example shows that the original list is updated by the method.
 
-def change_object(a_list):
+def change_list(a_list):
     a_list.append('another item')
     a_list.append('another item again')
 
 
 my_original_list = ['item1', 'item2']
 print(my_original_list)
-change_object(my_original_list)
+change_list(my_original_list)
+print(my_original_list)
+
+
+def change_another_list(a_list):
+    a_list = ['new', 'list']
+
+
+change_another_list(my_original_list)
+print(my_original_list)
+
+
+def change_another_list_wtih_return(a_list):
+    a_list = ['a', 'new', 'list']
+    return a_list
+
+
+my_original_list = change_another_list_wtih_return(my_original_list)
 print(my_original_list)
