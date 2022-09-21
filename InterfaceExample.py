ef1d869a3f2d2ff6d81a158f93797ce1eb19c08f
LiamHayes1 <liamhayes@live.ie>
2022-09-21 11:35:49 +0100

    Including a note and example of interfaces (which py does not have)

---
+++ InterfaceExample.py
@@ -0,0 +1,32 @@
+import time
+
+
+class Catapult:
+    def fire(self):
+        pass
+
+    def reload(self):
+        pass
+
+
+class IntergalacticCatapult(Catapult):
+    def __init__(self):
+        print('You have a new inter galactic catapult. ')
+        self.light_year_range = 100
+        self.secs_to_reload = 2
+
+    def fire(self):
+        print('Firing now! Range: ' + str(self.light_year_range))
+        print('Reloading time in seconds: ' + str(self.secs_to_reload))
+
+    def reload(self):
+        print('Reloading...')
+        time.sleep(self.secs_to_reload)
+        print('Reload complete. Ready to fire.')
+
+
+cata = IntergalacticCatapult()
+cata.fire()
+cata.reload()
+print('\nPython does not have interfaces, so I used an abstract class to do this, but it is not perfect. \n'
+      'Python used an ABC module to incorporate interface functionality, I should check that out.')
