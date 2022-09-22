cdc46abaf042c6c726097c2610a3a5f45bc19a9d
LiamHayes1 <liamhayes@live.ie>
2022-09-22 11:11:29 +0100

    add ABC (Abstract Base Class) example

---
+++ AbstractClasses.py
@@ -0,0 +1,29 @@
+# Abstract class - template class that cannot be instantiated
+# Abstract method - method defined but is not implemented
+
+from abc import ABC, abstractmethod
+
+
+class Truck(ABC):
+    @abstractmethod
+    def price(self):
+        pass
+
+
+class CementTruck(Truck):
+    def price(self):
+        print('120E per hour')
+
+
+class ArticulatedTruck(Truck):
+    def price(self):
+        print('100E per hour')
+
+
+truck1 = CementTruck()
+truck1.price()
+truck2 = ArticulatedTruck()
+truck2.price()
+
+# Trying to create a Truck object will result in an error
+truck3 = Truck()
