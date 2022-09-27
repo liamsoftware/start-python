32229422472944c78bb2adc771cd877f7dcdc1ea
LiamHayes1 <liamhayes@live.ie>
2022-09-27 14:40:39 +0100

    ordered dict for solving LRU cache

---
+++ LruCacheOrderDict.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+
+cache = LRUCache(2)
+cache.put(1, 1)
+cache.put(2, 2)
+cache.put(3, 3)
+cache.put(4, 4)
+print(cache.cache)
+
+cache.put(5, 5)
+print(cache.cache)
