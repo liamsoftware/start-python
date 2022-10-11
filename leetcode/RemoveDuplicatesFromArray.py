2afdb214e3606337af0252aaf438a7a23c4af5b2
LiamHayes1 <liamhayes@live.ie>
2022-10-11 11:05:09 +0100

    Add leetcode remove duplicates

---
+++ leetcode/RemoveDuplicatesFromArray.py
@@ -0,0 +1,8 @@
+def removeDuplicates(nums) -> int:
+    left = 1
+
+    for r in range(1, len(nums)):
+        if nums[r] != nums[r - 1]:
+            nums[left] = nums[r]
+            left += 1
+    return left
