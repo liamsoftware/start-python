af1b3ea9ceccf1aef49f7b012f51b14e74c8b7ea
LiamHayes1 <liamhayes@live.ie>
2022-10-12 10:36:10 +0100

    add ordering test

---
+++ ListAndTupleOrdering.py
@@ -0,0 +1,15 @@
+
+
+alist = []
+alist.append(4)
+alist.append(2)
+alist.append(3)
+print(alist)
+
+atuple = (4,2,3)
+print(atuple)
+
+alist.sort()
+print(alist)
+
+# Lists keep the insert order, can be sorted.
